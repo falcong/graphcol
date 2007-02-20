cf41393e5af8b84f6ca03996f29287baf2122d7c
pisciu <pisciu@82e17ac9-af26-0410-8529-519470a1cb1c>
2007-02-20 07:34:45 +0000

    Prima parte di istanze prodotte su desktop. Da correggere alcune deallocazioni mancate che provocano un sovraccarico di memoria nel VNS.

---
+++ trunk/batch.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+./color instances/DSJC250.5.col
+./color instances/DSJC500.5.col
+./color instances/DSJC1000.5.col
+./color instances/flat1000_76_0.col
+./color instances/flat300_28_0.col
+./color instances/le450_15c.col
+./color instances/le450_25c.col
+
