a85f47485d2c2dfca1c4e8217dc4510d180b5211
pisciu <pisciu@82e17ac9-af26-0410-8529-519470a1cb1c>
2007-02-21 12:05:58 +0000

    Nuovi test aggiunti.

--- trunk/batch.sh
+++ trunk/batch.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-./color instances/DSJC250.5.col
-./color instances/DSJC500.5.col
-./color instances/DSJC1000.5.col
+./color instances/DSJC250.5.col 28
+./color instances/DSJC500.5.col 50
+./color instances/DSJC1000.5.col 91
 ./color instances/flat1000_76_0.col
 ./color instances/flat300_28_0.col
 ./color instances/le450_15c.col
