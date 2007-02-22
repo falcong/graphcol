d1128d7e83ab384fbb2d871881dcda47b578c13a
pisciu <pisciu@82e17ac9-af26-0410-8529-519470a1cb1c>
2007-02-22 06:51:03 +0000

    Calcolate nuove istanze.

--- trunk/batch_vns.sh
+++ trunk/batch_vns.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-./color instances/DSJC250.5.col 28
-#./color instances/DSJC500.5.col 50
-#./color instances/DSJC1000.5.col 91
-./color instances/flat1000_76_0.col
-./color instances/flat300_28_0.col
-#BEST FINDED ./color instances/le450_15c.col
 ./color instances/le450_25c.col
+./color instances/flat300_28_0.col
+./color instances/flat1000_76_0.col
+./color instances/DSJC250.5.col 28
+./color instances/DSJC500.5.col 50
+./color instances/DSJC1000.5.col 91
+./color instances/flat1000_76_0.col
+#BEST FINDED ./color instances/le450_15c.col
 
